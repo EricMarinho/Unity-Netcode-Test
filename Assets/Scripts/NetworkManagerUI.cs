a1369dbed02a8a3f4ae8c263e9d8e6337fed91f9
Eric Marinho (Ilidam) <ericmarinho@acad.ifma.edu.br>
2023-01-27 16:01:37 -0300

    feat: Network UI added

---
+++ Assets/Scripts/NetworkManagerUI.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkManagerUI : MonoBehaviour
+{
+
+    [SerializeField] private Button severButton;
+    [SerializeField] private Button clientButton;
+    [SerializeField] private Button hostButton;
+
+}
