581db18fd1552a46ba69f1e025d82295f1848174
Eric Marinho (Ilidam) <ericmarinho@acad.ifma.edu.br>
2023-01-27 18:09:35 -0300

    feat: Added Network Variables

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -6,9 +6,40 @@ using UnityEngine;
 public class PlayerNetwork : NetworkBehaviour
 {
 
+    private NetworkVariable<MyCustomData> randomNumber = new NetworkVariable<MyCustomData>(
+        new MyCustomData
+        {
+            _int = 0,
+            _bool = false
+        }, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+    public struct MyCustomData
+    {
+        public int _int;
+        public bool _bool;
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        randomNumber.OnValueChanged += (MyCustomData previousValue, MyCustomData newValue) =>
+        {
+            Debug.Log(OwnerClientId + "; randomNumber: " + newValue._int + "; " + newValue._bool);
+        };
+    }
+
     private void Update()
     {
+
         if (!IsOwner) return;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            randomNumber.Value = new MyCustomData
+            {
+                _int = Random.Range(0, 100),
+                _bool = Random.Range(0, 2) == 0,
+            };
+        }
 
         Vector2 moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         transform.Translate(moveDirection * Time.deltaTime * 5f);
